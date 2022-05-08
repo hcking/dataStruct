72b27eff7cca327fbde50877f18670eb1b90ca0f
hcking <1109636124@qq.com>
2022-05-08 22:21:49 +0800

    引入assert包

---
+++ sort.go
@@ -0,0 +1,12 @@
+package dataStruct
+
+func Reverse(arr1 []int) []int {
+	n := len(arr1)
+	for i := 0; i < n/2; i++ {
+		tmp := arr1[i]
+		oppIndex := n - i - 1
+		arr1[i] = arr1[oppIndex]
+		arr1[oppIndex] = tmp
+	}
+	return arr1
+}
---
+++ sort_test.go
@@ -0,0 +1,13 @@
+package dataStruct
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	arr0 := []int{5, 4, 3, 2, 1}
+	arr1 := []int{1, 2, 3, 4, 5}
+	arr2 := Reverse(arr1)
+	assert.Equal(t, arr0, arr2)
+}
